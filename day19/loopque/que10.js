aa12f70d81f17df7cadc25d9222c5a05e7742456
Nitukumari12 <nitiyaraj528@gmail.com>
2023-08-17 19:40:50 +0530

    arrayque

---
+++ day19/loopque/que10.js
@@ -0,0 +1,9 @@
+// Modify an array of numbers using the forEach loop to double each value in place.
+
+// let arr = [2, 4, 6, 8];
+// arr.forEach((element, index) => { 
+//     arr[index] = element * 2; 
+// });
+// console.log(arr);
+
+
---
+++ day19/loopque/que5.js
@@ -0,0 +1,10 @@
+// Given an object person with properties name, age, and city, use a for...in loop to display all key-value pairs.
+
+// const item = {
+//    name:"nitu",
+//    age: 20,
+//    city:"Bhopal"
+// };
+// for(const key in item){
+//     console.log(`${key} ${item[key]}`);
+// }

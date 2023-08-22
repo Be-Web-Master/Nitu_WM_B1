78e66b2ba4f2a30c0969331b51187dd525b47263
Nitukumari12 <nitiyaraj528@gmail.com>
2023-08-22 11:23:57 +0530

    some que

---
+++ script.js
@@ -0,0 +1,10 @@
+// que1 Create a button element. When the button is clicked, change the text of the button to "Clicked!" (Event: click).
+
+// let btn = document.querySelector(".btn")
+// btn.addEventListener("click" ,  () =>{
+//     btn.innerHTML="clicked"
+// })
+
+
+// que2 Design a form with a single input field and a submit button. When the form is submitted, display an alert with the text entered in the input field (Event: submit).
+

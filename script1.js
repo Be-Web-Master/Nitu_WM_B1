fe1dffaf03988a5f8fb842e08c55999f2aa268a9
Nitukumari12 <nitiyaraj528@gmail.com>
2023-09-25 13:04:56 +0530

    aue3

--- script1.js
+++ script1.js
@@ -4,7 +4,7 @@
 const arr = [11,34,22,21,5,22]
  const find = 21;
 function number (arr,num) {
-    for(let i = 0;i<arr.length;i +=1){
+    for(let i = 0; i<arr.length; i +=1){
         if(arr[i] === num) return true;
     }
     return false;

e26fb5a263b1b9e0eab9ad64fd804358853ab325
Nitukumari12 <nitiyaraj528@gmail.com>
2023-09-25 13:06:54 +0530

    change

--- script2.js
+++ script2.js
@@ -1,9 +1,9 @@
 // "3. Reverse an array. [1,2,3,4,5,6] -> [6,5,4,3,2,1] (do not use built-in method)
 
-let arr = [1,2,3,4,5,6]
+let original = [1,2,3,4,5,6]
 let reverse =[]
 
-    for(let i = arr.length-1;i>=0;i--){
-        reverse.push(arr[i]);
+    for(let i = original.length-1;i>=0;i--){
+        reverse.push(original[i]);
     }
 console.log(reverse);
